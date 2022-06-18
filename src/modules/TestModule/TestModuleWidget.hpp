4718ed904e27487e614bd0197fae81a5034917e7
Patrick Gostovic <pg@phranq.com>
2022-06-18 06:00:35 -0400

    Moved ui widgets.

--- src/modules/TestModule/TestModuleWidget.hpp
+++ src/modules/TestModule/TestModuleWidget.hpp
@@ -16,15 +16,16 @@ struct TestModuleWidget : rack::app::ModuleWidget
 
     setPanel(createPanel(asset::plugin(pluginInstance, "res/TestModule.svg")));
 
-    // addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
-    // addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
-    // addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
-    // addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+    addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
+    addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
+    addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
+    addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
     // NOTE: when this widget is used to generate the module selector previes, module will be NULL.
-    addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(5.161, 15.6)), module, 0));
-    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(5.161, 26.239)), module, 0));
-    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(5.161, 36.817)), module, 0));
+    addParam(createParamCentered<RoundBlackKnob>(mm2px(Vec(15.24, 46)), module, 0));
+    addInput(createInputCentered<PJ301MPort>(mm2px(Vec(15.24, 77.478)), module, 0));
+    addOutput(createOutputCentered<PJ301MPort>(mm2px(Vec(15.24, 108.713)), module, 0));
+    // addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(15.24, 25.81)), module, 0));
 
     if (module)
     {
@@ -32,8 +33,6 @@ struct TestModuleWidget : rack::app::ModuleWidget
       module->addParamMapping(0, module->getDelegate()->pitchParam);
       module->addAudioOutputMapping(0, module->getDelegate()->audioOut);
     }
-
-    // addChild(createLightCentered<MediumLight<RedLight>>(mm2px(Vec(5.161, 8.741)), module, 0));
   }
 };
 
